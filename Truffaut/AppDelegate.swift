8403c4da7a345b06d90934310d7e1e4cffbf682f
Yan Li <eyeplum@gmail.com>
2016-04-14 21:29:56 +0800

    Initial commit

---
+++ Truffaut/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Truffaut
+//
+//  Created by Yan Li on 4/14/16.
+//  Copyright © 2016 Codezerker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+}
+
