366dbf5aa773b3ec2b5bc94098e515934eaac39f
Yan Li <eyeplum@gmail.com>
2017-04-03 22:09:11 +1200

    Update mock data

--- Mock Data/mock_data.swift
+++ Mock Data/mock_data.swift
@@ -1,29 +1,16 @@
-let slides = Slides(pages: [
-	// Cover
-	Cover(title: "Testing Swift", bulletPoints: ["Swift Parsing"]),
-	
-	// Page 1
-	Page(title: "Hello 1", bulletPoints: [
-			"Point 1.1",
-			"Point 1.2",
-			"Point 1.3",
-			"Point 1.4",
-			"Point 1.5",
-			]),
+import TruffautSupport
 
-	// Page 2
-	Page(title: "Hello 2", bulletPoints: [
-			"Point 2.1",
-			"Point 2.2",
-			"Point 2.3",
-			"Point 2.4",
-			"Point 2.5",
-			]),
-
-  // Tests
-  Page(title: "中文内容测试", bulletPoints: [
-    "Unicode is great",
-    "String is hard",
-    "中文处理很难",
-  ]),
-])
+let presentation = Presentation(
+   title: "presentation title",
+   authors: ["Yan Li <eyeplum@gmail.com>", "@eyeplum"],
+   pages: [
+       Page(template: "default",
+           title: "hello",
+           subtitle: "say hello",
+           contents: [
+               .bulletPoints([
+                   .image("./image/url.png"),
+                   .text("hello"),
+               ]),
+           ]),
+   ])
