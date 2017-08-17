02f26ac9a15394585f5e48bffb1c7d139ba96e21
Yan Li <eyeplum@gmail.com>
2017-08-17 22:55:46 +1200

    tweak style

--- Mock Data/mock_data.swift
+++ Mock Data/mock_data.swift
@@ -54,7 +54,7 @@ let presentation = Presentation(
                    super.viewDidLoad()
                    setUpViews()
                }
-           }       
+           }
            """),
        ])
    ])
